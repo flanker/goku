eec3525316a3e23994efdcb622606d6b083a716e
FENG Zhichao <flankerfc@gmail.com>
2014-05-14 02:08:37 +1000

    Add reading position indicator in post page.

---
+++ assets/javascripts/progress-bar.js
@@ -0,0 +1,20 @@
+$(document).on('ready', function() {
+
+  if ($('body.post').size() == 0) {
+    return;
+  }
+
+  var winHeight = $(window).height(),
+    docHeight = $(document).height(),
+    progressBar = $('progress'),
+    max, value;
+
+  /* Set the max scrollable area */
+  max = docHeight - winHeight;
+  progressBar.attr('max', max);
+
+  $(document).on('scroll', function(){
+    value = $(window).scrollTop();
+    progressBar.attr('value', value);
+  });
+});
