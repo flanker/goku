cb8168d78d03bebd9066974da01fc492bb19ecd2
FENG Zhichao <flankerfc@gmail.com>
2015-07-12 21:56:25 +1000

    Disable scrolling and change title

--- assets/javascripts/movies/movie.js
+++ assets/javascripts/movies/movie.js
@@ -1,11 +1,19 @@
 $(function() {
+  var $movieInfo = $('.movie-info');
+  var height = $movieInfo.outerHeight();
+  setTimeout(function() {
+    $movieInfo.css('bottom', '0px');
+  }, 500)
+
   $('body').tap(function() {
-    var $movieInfo = $('.movie-info');
-    var height = $movieInfo.outerHeight();
     if (parseInt($movieInfo.css('bottom')) == 0) {
       $movieInfo.css('bottom', '-' + height + 'px');
     } else {
       $movieInfo.css('bottom', '0px');
     }
   });
+
+  $('body').on('touchmove', function (event) {
+    event.preventDefault();
+  }, false);
 });
