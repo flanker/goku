b41ce01bd20222d77ba4e576a8de689994490fab
FENG Zhichao <flankerfc@gmail.com>
2015-07-12 12:51:40 +1000

    Show movie information

--- assets/javascripts/movies/movie.js
+++ assets/javascripts/movies/movie.js
@@ -1,1 +1,11 @@
-var a = 1;
+$(function() {
+  $('body').tap(function() {
+    var $movieInfo = $('.movie-info');
+    var height = $movieInfo.outerHeight();
+    if (parseInt($movieInfo.css('bottom')) == 0) {
+      $movieInfo.css('bottom', '-' + height + 'px');
+    } else {
+      $movieInfo.css('bottom', '0px');
+    }
+  });
+});
