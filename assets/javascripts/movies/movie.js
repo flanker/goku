809e30cb914fb9ced8c41e2308af5e8cb269b0d0
FENG Zhichao <flankerfc@gmail.com>
2015-07-14 08:12:44 +1000

    Replace touch events framework.

--- assets/javascripts/movies/movie.js
+++ assets/javascripts/movies/movie.js
@@ -5,15 +5,17 @@ $(function() {
     $movieInfo.css('bottom', '0px');
   }, 500)
 
-  $('body').tap(function() {
+  $('body').tap(function (e) {
     if (parseInt($movieInfo.css('bottom')) == 0) {
       $movieInfo.css('bottom', '-' + height + 'px');
     } else {
       $movieInfo.css('bottom', '0px');
     }
+  }).swiperight(function (e) {
+    $('.poster').append('swipe right; ')
+  }).swipeleft(function (e) {
+    $('.poster').append('swipe left; ')
+  }).scrollstart(function (e) {
+    e.preventDefault();
   });
-
-  $('body').on('touchmove', function (event) {
-    event.preventDefault();
-  }, false);
 });
