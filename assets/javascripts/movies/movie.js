4ceaf7611ae0880d756b64363ce4a5946d3a936f
FENG Zhichao <flankerfc@gmail.com>
2015-07-14 18:36:01 +1000

    Add hint for swipe to show more

--- assets/javascripts/movies/movie.js
+++ assets/javascripts/movies/movie.js
@@ -1,11 +1,18 @@
 $(function() {
   var $movieInfo = $('.movie-info');
   var height = $movieInfo.outerHeight();
+
   setTimeout(function() {
     $movieInfo.css('bottom', '0px');
   }, 500)
 
-  $('body').tap(function (e) {
+  setTimeout(function() {
+    $('.swipe-hint').fadeOut(1000);
+  }, 4000)
+
+  $('body').tapstart(function () {
+    $('.swipe-hint').fadeOut(300);
+  }).tap(function (e) {
     if (parseInt($movieInfo.css('bottom')) == 0) {
       $movieInfo.css('bottom', '-' + height + 'px');
     } else {
